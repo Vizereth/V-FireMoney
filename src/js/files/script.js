1f52faf6a5188ca88c7acdf1ece3068011a551fb
Vladislav Iskakov <71043551+Sophrona@users.noreply.github.com>
2023-12-01 03:21:04 +0800

    clean up

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,15 +1,8 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from './functions.js';
-// Подключение списка активных модулей
-import { flsModules } from './modules.js';
-
 import { initMenu } from './menu.js';
 import { initQuickFormSliders } from '../../modules/quick-form/initQuickFormSlider.js';
 import { initQuestionCards } from './modules/initQuestionCards.js';
 
 import './utilities.js';
-
-
 
 window.addEventListener('DOMContentLoaded', () => {
   initMenu();
